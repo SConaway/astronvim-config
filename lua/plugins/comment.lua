5e23aff84ffb35ff1057ac9151844ccb6e3da443
Steven Conaway <steven.conaway@icloud.com>
2024-04-22 18:46:37 -0400

    add verilog/systemverilog comments
    there is an existing PR to Comment.nvim but it hasn't been touched since
    creation

---
+++ lua/plugins/comment.lua
@@ -0,0 +1,11 @@
+return {
+  "numToStr/Comment.nvim",
+  init = function()
+    local ft = require "Comment.ft"
+
+    ---@diagnostic disable-next-line: param-type-mismatch
+    ft.set("systemverilog", ft.get "c")
+    ---@diagnostic disable-next-line: param-type-mismatch
+    ft.set("verilog", ft.get "c")
+  end,
+}
