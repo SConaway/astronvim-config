137d9b3c91e2cf1fc93b7edb0ada5a0cf178b4a4
Steven Conaway <steven.conaway@icloud.com>
2026-02-23 23:12:55 -0800

    stuff?

--- lua/community.lua
+++ lua/community.lua
@@ -11,6 +11,7 @@ return {
   -- { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.yaml" },
   -- { import = "astrocommunity.pack.rust" },
